56761c504d33b595f314119a788b0d95f47fc749
danlinyu <danlinyu@gmail.com>
2026-06-28 04:28:32 -0400

    port: guard Unix-only Rinterface.h on Windows, declare R_CStackLimit directly

--- inlaprog/src/R-interface.c
+++ inlaprog/src/R-interface.c
@@ -7,12 +7,16 @@
 #include <omp.h>
 #include <unistd.h>
 
-#define CSTACK_DEFNS 1
 #include <R.h>
 #include <Rembedded.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
-#include <Rinterface.h>
+#if !defined(_WIN32)
+#       define CSTACK_DEFNS 1
+#       include <Rinterface.h>
+#else
+extern uintptr_t R_CStackLimit;				       /* Rinterface.h is Unix-only; R.dll still exports this symbol */
+#endif
 
 #include "GMRFLib/timer.h"
 extern char *GMRFLib_tmpdir;
